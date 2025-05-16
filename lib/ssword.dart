4ef3b6d7e0330469f3c3f4a04c15999bed2b5f4d
Ssword-dev <ssword.dev@gmail.com>
2025-05-16 14:29:16 +0800

    Exported Annotations

--- lib/ssword.dart
+++ lib/ssword.dart
@@ -5,4 +5,7 @@ library;
 
 export 'src/annotation_linter.dart';
 
+// ssword/annotations
+export 'src/annotations/internal.dart';
+
 // TODO: Export any libraries intended for clients of this package.
