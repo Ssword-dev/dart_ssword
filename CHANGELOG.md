5233ca187aabbec74cf818982aeebfd2914508a8
Ssword-dev <ssword.dev@gmail.com>
2025-05-16 20:45:48 +0800

    Update:1.0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,6 @@
 
 ## 1.0.\[1-4\]
 - Bug Fixes
+
+## 1.0.5
+- Bug Fixed @internal not working
--- lib/src/linter/internal.dart
+++ lib/src/linter/internal.dart
@@ -1,19 +1,18 @@
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/element2.dart';
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
-/// Implementation of @internal annotation
 class AtInternalRule extends DartLintRule {
   AtInternalRule()
     : super(
         code: LintCode(
           name: "at_internal",
           problemMessage:
-              "elements declared with mark @internal can only be used in the same file of its declaration!",
+              "Elements marked @internal can only be used in the same file they were declared.",
         ),
       );
+
   @override
   void run(
     CustomLintResolver resolver,
@@ -22,7 +21,6 @@ class AtInternalRule extends DartLintRule {
   ) {
     context.registry.addSimpleIdentifier((SimpleIdentifier node) {
       final Element? element = node.staticElement;
-
       if (element == null) return;
 
       if (element is! ExecutableElement &&
@@ -31,32 +29,24 @@ class AtInternalRule extends DartLintRule {
         return;
       }
 
-      // First, check if element is annotated with @internal, else, we dont care
-      // so we return early
+      final isInternal = element.metadata.any(
+        (annotation) =>
+            annotation.element?.name == 'internal' ||
+            annotation.element?.enclosingElement3?.displayName == 'internal',
+      );
 
-      final bool isInternal = element.metadata.any((
-        ElementAnnotation annotation,
-      ) {
-        return annotation.element?.name == "internal" ||
-            annotation.element?.enclosingElement3?.displayName == "internal";
-      });
+      if (!isInternal) return;
 
-      if (isInternal) {
-        // get the file where this element was declared
-        String? declarationFileName = element.declaration?.source?.shortName;
-        if (declarationFileName == null) {
-          return; // well, we have no source of the declaration to look at so... we cant trace it therefore we return
-        }
-        String? usedAt = element.source?.shortName;
-        if (usedAt == null) {
-          return; // again, where we used this is unknown so we return early
-        }
+      final usedAt =
+          node
+              .thisOrAncestorOfType<CompilationUnit>()
+              ?.declaredElement
+              ?.source
+              .fullName;
+      final declaredAt = element.declaration?.source?.fullName;
 
-        // check if the file that used this element is the same as the file that declares it
-
-        if (declarationFileName != usedAt) {
-          reporter.atNode(node, code);
-        }
+      if (usedAt != null && declaredAt != null && usedAt != declaredAt) {
+        reporter.atNode(node, code);
       }
     });
   }
